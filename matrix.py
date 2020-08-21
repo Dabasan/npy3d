b7e03ad457046f664a6a23b9e5945d77bd4ad3df
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-08-21 18:02:32 +0900

    Initial commit

---
+++ matrix.py
@@ -0,0 +1,188 @@
+"""
+4x4 matrix
+"""
+
+import numpy as np
+
+class Matrix(object):
+    """
+    4x4 matrix
+    """
+    def __init__(self,fill_value:float=None):
+        super().__init__()
+
+        if fill_value is None:
+            self.m=np.zeros((4,4))
+        else:
+            self.m=np.full((4,4),fill_value)
+
+    @classmethod
+    def from_matrix(cls,matrix:Matrix)->Matrix:
+        ret=Matrix()
+        for i in range(4):
+            for j in range(4):
+                ret.set(i,j,matrix.get(i,j))
+
+        return ret
+    @classmethod
+    def from_ndarray(cls,arr:np.ndarray)->Matrix:
+        if arr.shape!=(4,4):
+            message="Expected (4,4) array but got {}.".format(arr.shape)
+            raise ValueError(message)
+
+        ret=Matrix()
+        for i in range(4):
+            for j in range(4):
+                ret.set(i,j,arr[i,j])
+
+        return ret
+
+    def __str__(self):
+        ret=""
+        for i in range(4):
+            for j in range(4):
+                ret+=str(self.m[i,j])
+                ret+=" "
+            ret+="\n"
+
+        ret=ret[:len(ret)-1]
+
+        return ret
+
+    def get_ndarray(self)->np.ndarray:
+        """
+        Returns the underlying ndarray instance.
+        """
+        return self.m
+
+    def get(self,row:int,col:int)->float:
+        return self.m[row,col]
+    
+    def set(self,row:int,col:int,value:float):
+        self.m[row,col]=value
+
+    def add(self,mat:Matrix)->Matrix:
+        ret=Matrix()
+        for i in range(4):
+            for j in range(4):
+                v1=self.m[i,j]
+                v2=mat.m[i,j]
+
+                ret.set(i,j,v1+v2)
+        
+        return ret
+    def sub(self,mat:Matrix)->Matrix:
+        ret=Matrix()
+        for i in range(4):
+            for j in range(4):
+                v1=self.m[i,j]
+                v2=mat.m[i,j]
+
+                ret.set(i,j,v1-v2)
+        
+        return ret
+    def mult(self,mat:Matrix)->Matrix:
+        mult_arr=np.matmul(self.m,mat.m)
+        return Matrix.from_ndarray(mult_arr)
+
+    def transpose(self)->Matrix:
+        ret=Matrix()
+        for i in range(4):
+            for j in range(4):
+                ret.set(i,j,self.m[j,i])
+
+        return ret
+    def invert(self)->Matrix:
+        inv_arr=np.linalg.inv(self.m)
+        return Matrix.from_ndarray(inv_arr)
+
+    @classmethod
+    def identity(cls)->Matrix:
+        ret=Matrix(0.0)
+        for i in range(4):
+            ret.set(i,i,1.0)
+
+        return ret
+    @classmethod
+    def random(cls)->Matrix:
+        rand_arr=np.random.rand(4,4)
+        return Matrix.from_ndarray(rand_arr)
+    @classmethod
+    def translaion(cls,translation_x:float,translation_y:float,translation_z:float)->Matrix:
+        ret=Matrix(0.0)
+        ret.set(0,0,1.0)
+        ret.set(0,3,translation_x)
+        ret.set(1,1,1.0)
+        ret.set(1,3,translation_y)
+        ret.set(2,2,1.0)
+        ret.set(2,3,translation_z)
+        ret.set(3,3,1.0)
+
+        return ret
+    @classmethod
+    def scaling(cls,scale_x:float,scale_y:float,scale_z:float)->Matrix:
+        ret=Matrix(0.0)
+        ret.set(0,0,scale_x)
+        ret.set(1,1,scale_y)
+        ret.set(2,2,scale_z)
+        ret.set(3,3,1.0);
+
+        return ret
+    @classmethod
+    def rotation_x(cls,th:float)->Matrix:
+        ret=Matrix(0.0)
+        ret.set(0, 0, 1.0)
+        ret.set(1,1,math.cos(th))
+        ret.set(1,2,-math.sin(th))
+        ret.set(2,1,math.sin(th))
+        ret.set(2,2,math.cos(th))
+        ret.set(3,3,1.0)
+
+        return ret
+    @classmethod
+    def rotation_y(cls,th:float)->Matrix:
+        ret=Matrix(0.0)
+        ret.set(0,0,math.cos(th))
+        ret.set(0,2,math.sin(th))
+        ret.set(1,1,1.0)
+        ret.set(2,0,-math.sin(th))
+        ret.set(2,2,math.cos(th))
+        ret.set(3,3,1.0)
+
+        return ret
+    @classmethod
+    def rotation_z(cls,th:float)->Matrix:
+        ret=Matrix(0.0)
+        ret.set(0,0,math.cos(th))
+        ret.set(0,1,-math.sin(th))
+        ret.set(1,0,math.sin(th))
+        ret.set(1,1,math.cos(th))
+        ret.set(2,2,1.0)
+        ret.set(3,3,1.0)
+
+        return ret
+    @classmethod
+    def rotation(cls,axis_x:float,axis_y:float,axis_z:float,th:float)->Matrix:
+        cos_th=math.cos(th)
+        sin_th=math.sin(th)
+        
+        ret=Matrix()
+        ret.set(0,0,cos_th+axis_x*axis_x*(1.0-cos_th))
+        ret.set(0,1,axis_x*axis_y*(1.0-cos_th)-axis_z*sin_th)
+        ret.set(0,2,axis_x*axis_z*(1.0-cos_th)+axis_y*sin_th)
+        ret.set(0,3,0.0)
+        ret.set(1,0,axis_y*axis_x*(1.0-cos_th)+axis_z*sin_th)
+        ret.set(1,1,cos_th+axis_y*axis_y*(1.0-cos_th))
+        ret.set(1,2,axis_y*axis_z*(1.0-cos_th)-axis_x*sin_th)
+        ret.set(1,3,0.0)
+        ret.set(2,0,axis_z*axis_x*(1.0-cos_th)-axis_y*sin_th)
+        ret.set(2,1,axis_z*axis_y*(1.0-cos_th)+axis_x*sin_th)
+        ret.set(2,2,cos_th+axis_z*axis_z*(1.0-cos_th))
+        ret.set(2,3,0.0)
+        ret.set(3,0,0.0)
+        ret.set(3,1,0.0)
+        ret.set(3,2,0.0)
+        ret.set(3,3,1.0)
+
+        return ret
+    
---
+++ vector.py
@@ -0,0 +1,180 @@
+"""
+Vector
+"""
+
+import numpy as np
+import math
+from typing import Tuple
+
+from matrix import Matrix
+
+class Vector(object):
+    """
+    Vector
+    """
+    def __init__(self,init_values:Tuple[float,float,float]=None):
+        super().__init__()
+
+        if init_values is None:
+            self.v=np.zeros((4,1))
+        else:
+            self.v=np.empty((4,1))
+            self.v[0,0]=init_values[0]
+            self.v[1,0]=init_values[1]
+            self.v[2,0]=init_values[2]
+
+        self.v[3,0]=1.0
+
+    @classmethod
+    def from_angles(cls,angle_v:float,angle_h:float)->Vector:
+        x=math.cos(angle_h)
+        y=math.sin(angle_v)
+        z=-math.sin(angle_h)
+        size=math.sqrt(x*x+y*y+z*z)
+
+        ret=Vector()
+        ret.set(x,y,z)
+
+        return ret
+    @classmethod
+    def from_vector(cls,v:Vector)->Vector:
+        ret=Vector()
+        ret.set(v.get_x(),v.get_y(),v.get_z())
+
+        return ret
+    @classmethod
+    def from_ndarray(cls,arr:np.ndarray)->Vector:
+        if arr.shape!=(3,1):
+            message="Expected (3,1) but got {}.".format(arr.shape)
+            raise ValueError(message)
+
+        ret=Vector()
+        ret.set(arr[0,0],arr[1,0],arr[2,0])
+
+        return ret
+
+    def __str__(self):
+        return "({},{},{})".format(self.v[0,0],self.v[1,0],self.v[2,0])
+
+    def get(self)->Tuple[float,float,float]:
+        return self.v[0,0],self.v[1,0],self.v[2,0]
+    def get_x(self)->float:
+        return self.v[0,0]
+    def get_y(self)->float:
+        return self.v[1,0]
+    def get_z(self)->float:
+        return self.v[2,0]
+
+    def set(self,x:float,y:float,z:float):
+        self.v[0,0]=x
+        self.v[1,0]=y
+        self.v[2,0]=z
+    def set_x(self,value):
+        self.v[0,0]=value
+    def set_y(self,value):
+        self.v[1,0]=value
+    def set_z(self,value):
+        self.v[2,0]=value
+
+    def get_square_size(self)->float:
+        x=self.v[0,0]
+        y=self.v[1,0]
+        z=self.v[2,0]
+
+        return x*x+y*y+z*z
+    def get_size(self)->float:
+        x=self.v[0,0]
+        y=self.v[1,0]
+        z=self.v[2,0]
+
+        return math.sqrt(x*x+y*y+z*z)
+
+    def normalize(self)->Vector:
+        size=self.get_size()
+
+        x=self.v[0,0]/size
+        y=self.v[1,0]/size
+        z=self.v[2,0]/size
+
+        return Vector((x,y,z))
+
+    def add(self,v:Vector)->Vector:
+        x=self.get_x()+v.get_x()
+        y=self.get_y()+v.get_y()
+        z=self.get_z()+v.get_z()
+
+        return Vector((x,y,z))
+    def sub(self,v:Vector)->Vector:
+        x=self.get_x()-v.get_x()
+        y=self.get_y()-v.get_y()
+        z=self.get_z()-v.get_z()
+
+        return Vector((x,y,z))
+    def cross(self,v:Vector)->Vector:
+        a1=self.get_x()
+        a2=self.get_y()
+        a3=self.get_z()
+        b1=v.get_x()
+        b2=v.get_y()
+        b3=v.get_z()
+
+        x=a2*b3-b2*a3
+        y=a3*b1-b3*a1
+        z=a1*b2-b1*a2
+
+        return Vector((x,y,z))
+    def dot(self,v:Vector)->float:
+        x1=self.get_x()
+        y1=self.get_y()
+        z1=self.get_z()
+        x2=v.get_x()
+        y2=v.get_y()
+        z2=v.get_z()
+
+        return x1*x2+y1*y2+z1*z2
+
+    def get_angle_v(self)->float:
+        xz_vec=Vector(self.get_x(),0.0,self.get_z())
+
+        cos_th=self.dot(xz_vec)/(self.get_size()*xz_vec.get_size())
+        th=math.acos(cos_th)
+
+        return th
+    def get_angle_h(self)->float:
+        x_axis=Vector(init_values=(1.0,0.0,0.0))
+        xz_vec=Vector(init_values=(self.get_x(),0.0,self.get_z()))
+
+        cos_th=xz_vec.dot(x_axis)/xz_vec.get_size()
+        th=math.acos(cos_th)
+
+        if self.get_z()>=0.0:
+            th*=(-1.0)
+
+        return th
+    
+    def transform(self,matrix:Matrix)->Vector:
+        transformed_arr=np.matmul(matrix.get_ndarray(),self.v)
+        return Vector.from_ndarray(transformed_arr)
+    def transform_sr(self,matrix:Matrix)->Vector:
+        mat_temp=np.empty((4,1))
+        mat_temp[0,0]=self.v[0,0]
+        mat_temp[1,0]=self.v[1,0]
+        mat_temp[2,0]=self.v[2,0]
+        mat_temp[3,0]=0.0
+
+        transformed_arr=np.matmul(matrix,mat_temp)
+
+        return Vector(transformed_arr)
+
+    def rot_x(self,th:float)->Vector:
+        rot_mat=Matrix.rotation_x(th)
+        return self.transform(rot_mat)
+    def rot_y(self,th:float)->Vector:
+        rot_mat=Matrix.rotation_y(th)
+        return self.transform(rot_mat)
+    def rot_z(self,th:float)->Vector:
+        rot_mat=Matrix.rotation_z(th)
+        return self.transform(rot_mat)
+    def rot(self,axis_x:float,axis_y:float,axis_z:float,th:float)->Vector:
+        rot_mat=Matrix.rotation(axis_x,axis_y,axis_z,th)
+        return self.transform(rot_mat)
