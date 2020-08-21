bb23247603440da1fd6ed49129d0106ab4f551f8
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-08-21 21:45:34 +0900

    Updated vector.py.

--- vector.py
+++ vector.py
@@ -26,18 +26,18 @@ class Vector(object):
         self.v[3,0]=1.0
 
     @classmethod
-    def from_angles(cls,angle_v:float,angle_h:float)->Vector:
+    def from_angles(cls,angle_v:float,angle_h:float)->"Vector":
         x=math.cos(angle_h)
         y=math.sin(angle_v)
         z=-math.sin(angle_h)
         size=math.sqrt(x*x+y*y+z*z)
 
-        return Vector((x,y,z))
+        return Vector((x/size,y/size,z/size))
     @classmethod
-    def from_vector(cls,v:Vector)->Vector:
+    def from_vector(cls,v:"Vector")->"Vector":
         return Vector((v.get_x(),v.get_y(),v.get_z()))
     @classmethod
-    def from_ndarray(cls,arr:np.ndarray)->Vector:
+    def from_ndarray(cls,arr:np.ndarray)->"Vector":
         if arr.shape!=(3,1) and arr.shape!=(4,1):
             message="Expected shape (3,1) or (4,1) but got {}.".format(arr.shape)
             raise ValueError(message)
@@ -54,7 +54,7 @@ class Vector(object):
         return self.v
 
     def get(self)->Tuple[float,float,float]:
-        return self.v[0,0],self.v[1,0],self.v[2,0]
+        return (self.v[0,0],self.v[1,0],self.v[2,0])
     def get_x(self)->float:
         return self.v[0,0]
     def get_y(self)->float:
@@ -86,7 +86,7 @@ class Vector(object):
 
         return math.sqrt(x*x+y*y+z*z)
 
-    def normalize(self)->Vector:
+    def normalize(self)->"Vector":
         size=self.get_size()
 
         x=self.v[0,0]/size
@@ -95,19 +95,19 @@ class Vector(object):
 
         return Vector((x,y,z))
 
-    def add(self,v:Vector)->Vector:
+    def add(self,v:"Vector")->"Vector":
         x=self.get_x()+v.get_x()
         y=self.get_y()+v.get_y()
         z=self.get_z()+v.get_z()
 
         return Vector((x,y,z))
-    def sub(self,v:Vector)->Vector:
+    def sub(self,v:"Vector")->"Vector":
         x=self.get_x()-v.get_x()
         y=self.get_y()-v.get_y()
         z=self.get_z()-v.get_z()
 
         return Vector((x,y,z))
-    def cross(self,v:Vector)->Vector:
+    def cross(self,v:"Vector")->"Vector":
         a1=self.get_x()
         a2=self.get_y()
         a3=self.get_z()
@@ -120,7 +120,7 @@ class Vector(object):
         z=a1*b2-b1*a2
 
         return Vector((x,y,z))
-    def dot(self,v:Vector)->float:
+    def dot(self,v:"Vector")->float:
         x1=self.get_x()
         y1=self.get_y()
         z1=self.get_z()
@@ -149,10 +149,10 @@ class Vector(object):
 
         return th
     
-    def transform(self,matrix:Matrix)->Vector:
+    def transform(self,matrix:Matrix)->"Vector":
         transformed_arr=np.matmul(matrix.get_ndarray(),self.v)
         return Vector.from_ndarray(transformed_arr)
-    def transform_sr(self,matrix:Matrix)->Vector:
+    def transform_sr(self,matrix:Matrix)->"Vector":
         mat_temp=np.empty((4,1))
         mat_temp[0,0]=self.v[0,0]
         mat_temp[1,0]=self.v[1,0]
@@ -163,15 +163,15 @@ class Vector(object):
 
         return Vector(transformed_arr)
 
-    def rot_x(self,th:float)->Vector:
+    def rot_x(self,th:float)->"Vector":
         rot_mat=Matrix.rotation_x(th)
         return self.transform(rot_mat)
-    def rot_y(self,th:float)->Vector:
+    def rot_y(self,th:float)->"Vector":
         rot_mat=Matrix.rotation_y(th)
         return self.transform(rot_mat)
-    def rot_z(self,th:float)->Vector:
+    def rot_z(self,th:float)->"Vector":
         rot_mat=Matrix.rotation_z(th)
         return self.transform(rot_mat)
-    def rot(self,axis_x:float,axis_y:float,axis_z:float,th:float)->Vector:
+    def rot(self,axis_x:float,axis_y:float,axis_z:float,th:float)->"Vector":
         rot_mat=Matrix.rotation(axis_x,axis_y,axis_z,th)
         return self.transform(rot_mat)
