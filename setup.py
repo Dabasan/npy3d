260a09faa9c24d6e5003a97d48b88cc37ebb6713
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-08-22 17:50:06 +0900

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="npy3d",
+    version="0.0.1",
+    author="Hirotsugu Daba",
+    author_email="N/A",
+    description="A Python library for 3D vector and matrix operations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dabasan/npy3d",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
